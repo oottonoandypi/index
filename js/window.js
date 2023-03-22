bf03c2e0a64d8defa0fe2cd1b101eac4a5279884
yunyisilva <yunyid8+github@gmail.com>
2023-03-22 10:40:46 -0700

    Filled in content and fixed the window_content_header_nav stay on top as scroll.

--- js/window.js
+++ js/window.js
@@ -1,9 +1,16 @@
 let addStrong = function(ele){
     ele.innerHTML = "<strong>"+ele.innerHTML+"</strong>";
-}
+};
 
 let removeStrong = function(ele, str){
     setTimeout(function(){
         ele.innerHTML=str;
     }, 100);
+};
+
+let disableEle = function(ele){
+    document.getElementById("window_content_header_nav_top").disabled=false;
+    document.getElementById("window_content_header_nav_portfolio").disabled=false;
+    document.getElementById("window_content_header_nav_about").disabled=false;
+    ele.disabled=true;
 }
