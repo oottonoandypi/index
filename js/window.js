1ec5407f5ab7f9ca447f2c1d02e46c030c5ecc5a
yunyisilva <yunyid8+github@gmail.com>
2023-03-22 15:58:06 -0700

    Fixed issue: button click off center.

--- js/window.js
+++ js/window.js
@@ -1,33 +1,55 @@
 const windowCanvas = document.getElementById("window_content_canvas");
+const nav_portfolio=document.getElementById("window_content_header_nav_portfolio");
+const nav_me=document.getElementById("window_content_header_nav_me");
+const nav_top=document.getElementById("window_content_header_nav_top");
+let currDisabled = null;
 
 let addStrong = function(ele){
     ele.innerHTML = "<strong>"+ele.innerHTML+"</strong>";
 };
 
 let removeStrong = function(ele, str){
-    ele.innerHTML=str;
+    setTimeout(function(){
+        ele.innerHTML=str;
+    }, 0);
 };
 
 let disableEle = function(ele){
-    document.getElementById("window_content_header_nav_top").disabled=false;
-    document.getElementById("window_content_header_nav_portfolio").disabled=false;
-    document.getElementById("window_content_header_nav_about").disabled=false;
-    ele.disabled=true;
+    // document.getElementById("window_content_header_nav_top").disabled=false;
+     if(ele==nav_me) {
+        nav_top.disabled=false;
+        removeStrong(nav_top, "TOP");
+        nav_portfolio.disabled=false;
+        removeStrong(nav_portfolio, "PORTFOLIO");
+    }else if(ele==nav_portfolio){
+        nav_top.disabled=false;
+        removeStrong(nav_top, "TOP");
+        nav_me.disabled=false;
+        removeStrong(nav_me, "BEHIND OOTTONOANDYPI");
+    }else{
+        nav_portfolio.disabled=false;
+        removeStrong(nav_portfolio, "PORTFOLIO");
+        nav_me.disabled=false;
+        removeStrong(nav_me, "BEHIND OOTTONOANDYPI");
+    }
+
+    if(ele!=null) {
+        ele.disabled=true;
+        addStrong(ele);
+    }
 }
 
+disableEle(nav_top);
 windowCanvas.addEventListener("scroll", function() {
     var portfolioEle = document.getElementById("window_content_body_portfolio");
-    var aboutEle = document.getElementById("window_content_body_about");
+    var meEle = document.getElementById("window_content_body_me");
+    // var topEle = document.getElementById("window_content_body");
 
-    var nav_top=document.getElementById("window_content_header_nav_top");
-    var nav_portfolio=document.getElementById("window_content_header_nav_portfolio");
-    var nav_about=document.getElementById("window_content_header_nav_about");
-
-    if(windowCanvas.scrollTop > (aboutEle.offsetHeight+aboutEle.offsetTop-1000)) {
-        disableEle(nav_about);
-    }else if (windowCanvas.scrollTop > (portfolioEle.offsetHeight+portfolioEle.offsetTop-200)) {
-        disableEle(nav_portfolio);
+    if(windowCanvas.scrollTop > (meEle.offsetTop-100)) {
+        if(currDisabled!=nav_me) disableEle(nav_me);
+    }else if (windowCanvas.scrollTop > (portfolioEle.offsetTop-200)) {
+        if(currDisabled!=nav_portfolio) disableEle(nav_portfolio);
     }else{
-        disableEle(nav_top);
+        if(currDisabled!=nav_top) disableEle(nav_top);
     }
 });
