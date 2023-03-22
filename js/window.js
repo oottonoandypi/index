9e138d3570f733fcde039057a00801cafd8d848c
yunyisilva <yunyid8+github@gmail.com>
2023-03-22 11:30:50 -0700

    Fixed scroll style change to the windowwindow_content_header_nav buttons.

--- js/window.js
+++ js/window.js
@@ -1,11 +1,11 @@
+const windowCanvas = document.getElementById("window_content_canvas");
+
 let addStrong = function(ele){
     ele.innerHTML = "<strong>"+ele.innerHTML+"</strong>";
 };
 
 let removeStrong = function(ele, str){
-    setTimeout(function(){
-        ele.innerHTML=str;
-    }, 100);
+    ele.innerHTML=str;
 };
 
 let disableEle = function(ele){
@@ -14,3 +14,20 @@ let disableEle = function(ele){
     document.getElementById("window_content_header_nav_about").disabled=false;
     ele.disabled=true;
 }
+
+windowCanvas.addEventListener("scroll", function() {
+    var portfolioEle = document.getElementById("window_content_body_portfolio");
+    var aboutEle = document.getElementById("window_content_body_about");
+
+    var nav_top=document.getElementById("window_content_header_nav_top");
+    var nav_portfolio=document.getElementById("window_content_header_nav_portfolio");
+    var nav_about=document.getElementById("window_content_header_nav_about");
+
+    if(windowCanvas.scrollTop > (aboutEle.offsetHeight+aboutEle.offsetTop-1000)) {
+        disableEle(nav_about);
+    }else if (windowCanvas.scrollTop > (portfolioEle.offsetHeight+portfolioEle.offsetTop-200)) {
+        disableEle(nav_portfolio);
+    }else{
+        disableEle(nav_top);
+    }
+});
